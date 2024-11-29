c038149c5d882240e52f078fcd5bcab81b8eee41
Alan Lengkoan <alanlengkoan15@gmail.com>
2024-11-29 20:21:56 +0800

    update again

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const __filename = fileURLToPath(
     import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.use(expressEjsLayouts);
 
 app.set('view engine', 'ejs');
