8ad3b5aa2a1decd44686e1cd712623a51a40562f
Alan Lengkoan <alansaputralengkoan@gmail.com>
2024-12-06 15:12:23 +0800

    update agian

--- functions/server.js
+++ functions/server.js
@@ -3,6 +3,12 @@ import ServerlessHttp from "serverless-http";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.json({
+        "message": "Hello World"
+    });
+});
+
 app.get("/.netlify/functions/api", (req, res) => {
     res.json({
         "message": "Hello World"
