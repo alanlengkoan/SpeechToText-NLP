1d211b095a488e53fe59ceec2f54eef43e9ea26c
Alan Lengkoan <alansaputralengkoan@gmail.com>
2024-12-06 14:55:26 +0800

    update agian

--- functions/server.js
+++ functions/server.js
@@ -8,23 +8,10 @@ dotEnv.config({
 
 const app = express();
 
-app.get("/.netlify/functions/index", (req, res) => {
-    return res.json({
+app.get("/.netlify/functions/api", (req, res) => {
+    res.json({
         "message": "Hello World"
     });
 });
 
-if (process.env.APP_STAGE === 'dev') {
-    const port = process.env.PORT || 5000;
-    const host = process.env.HOST || 'localhost';
-
-    app.listen(port, host, () => {
-        console.log(`Server is running on http://${host}:${port}`);
-    });
-} else {
-    const server = ServerlessHttp(app);
-
-    module.exports.handler = async (event, context) => {
-        return await server(event, context);
-    }
-}
+ServerlessHttp(app);
