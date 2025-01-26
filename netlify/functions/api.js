93da688dbcca319005b92260a52d6bd7cf8a92e3
Alan Lengkoan <alanlengkoan15@gmail.com>
2025-01-27 01:53:20 +0800

    update again

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,15 +3,11 @@ import express, {
 } from "express";
 import expressEjsLayouts from "express-ejs-layouts";
 import path from "path";
-import {
-    fileURLToPath
-} from "url";
 import serverless from 'serverless-http';
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 app.use(expressEjsLayouts);
 
@@ -25,9 +21,21 @@ const router = Router();
 
 // untuk tampilkan halaman utama
 router.get("/", (req, res) => {
-    res.send(__dirname);
+     var data = {
+        halaman: 'Home',
+        layout: 'base',
+    };
+
+    res.render('home/view', data);
 });
 
 app.use('/api/', router);
 
 export const handler = serverless(app);
+
+// const port = process.env.PORT || 5000;
+// const host = process.env.HOST || 'localhost';
+
+// app.listen(port, host, () => {
+//     console.log(`Server is running on http://${host}:${port}/api`);
+// });
