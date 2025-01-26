782a67c8df93f12fcd69ea41d0f8f49a698df294
Alan Lengkoan <alanlengkoan15@gmail.com>
2025-01-27 00:55:20 +0800

    update again

---
+++ netlify/functions/api.js
@@ -0,0 +1,18 @@
+import express, {
+    Router
+} from "express";
+import serverless from 'serverless-http';
+
+const app = express();
+
+const router = Router();
+
+router.get("/", (req, res) => {
+    res.json({
+        message: "hello world"
+    });
+});
+
+app.use('/api/', router);
+
+export const handler = serverless(app);
