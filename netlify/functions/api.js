fadd954b5877c1ee66ce93f83a0d84faf340c0ca
Alan Lengkoan <alanlengkoan15@gmail.com>
2025-01-27 10:56:45 +0800

    update again

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -37,11 +37,11 @@ router.get("/voice", async (req, res) => {
 
 app.use('/api/', router);
 
-// export const handler = serverless(app);
+export const handler = serverless(app);
 
-const port = process.env.PORT || 5000;
-const host = process.env.HOST || 'localhost';
+// const port = process.env.PORT || 5000;
+// const host = process.env.HOST || 'localhost';
 
-app.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}/api`);
-});
+// app.listen(port, host, () => {
+//     console.log(`Server is running on http://${host}:${port}/api`);
+// });
