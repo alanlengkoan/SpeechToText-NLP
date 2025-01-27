346c3639c88749ee2eca398570392e38ffc7df14
Alan Lengkoan <alanlengkoan15@gmail.com>
2025-01-27 10:54:13 +0800

    update again

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,27 +1,47 @@
 import express, {
     Router
 } from "express";
+import cors from "cors";
 import path from "path";
+import { Voice } from "./models/voice.js";
 import serverless from 'serverless-http';
 
 const app = express();
 
 const __dirname = path.resolve();
 
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
 const router = Router();
 
 // untuk tampilkan halaman utama
 router.get("/", (req, res) => {
-    res.render('halaman utama');
+    res.send("halaman utama");
+});
+
+router.get("/voice", async (req, res) => {
+    const voice = await Voice();
+
+    res.json({
+        success: true,
+        message: "success",
+        data: voice
+    }, 200);
 });
 
 app.use('/api/', router);
 
-export const handler = serverless(app);
+// export const handler = serverless(app);
 
-// const port = process.env.PORT || 5000;
-// const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || 'localhost';
 
-// app.listen(port, host, () => {
-//     console.log(`Server is running on http://${host}:${port}/api`);
-// });
+app.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}/api`);
+});
