1bcf3142c53df34de083efa1437d64a7e9a6ecce
Alan Lengkoan <alanlengkoan15@gmail.com>
2025-03-16 17:00:17 +0800

    update again

--- learn/views.py
+++ learn/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+import os
 
 def index(request):
     return render(request, 'index.html')
@@ -9,12 +11,38 @@ def idtobu(request):
     return render(request, 'idtobu.html')
 
 def butoid(request):
-    return render(request, 'butoid.html')
+    media_path = settings.MEDIA_ROOT
+    files = os.listdir(media_path) if os.path.exists(media_path) else []
+    
+    return render(request, 'butoid.html', {"files": files})
 
 @csrf_exempt
 def detect(request):
+    data = request.POST
+    files = request.FILES
+    
+    # message = data.get('message')
+    file = files.get('file')
+
+    save_path = os.path.join(settings.MEDIA_ROOT, file.name)
+
+    with open(save_path, "wb+") as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+    
+    print(file.name)
+
     data = {
         "fulfillmentText": "Hello, this is a JSON response!",
         "status": "success"
     }
     return JsonResponse(data)
+
+def delete_file(request, filename):
+    file_path = os.path.join(settings.MEDIA_ROOT, filename)
+    
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        return redirect("list_files")  # Kembali ke daftar file setelah menghapus
+    else:
+        return HttpResponse("File tidak ditemukan.", status=404)
